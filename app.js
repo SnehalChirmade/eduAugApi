2c41ba85392fe6c65fae3dfe301d3fbade7fdac2
SnehalChirmade <snehal1061993@gmail.com>
2021-12-03 16:16:27 +0530

    menu modified

--- app.js
+++ app.js
@@ -101,15 +101,13 @@ app.get('/details/:id',(req,res) => {
 })
 
 // menu Details on basis for restaurant
-app.get('/menu/:id',(req,res) => {
-    var id = req.params.id
-    console.log(id)
-    db.collection('menu').find({restaurant_id:Number(id)}).toArray((err,result)=>{
+app.post('/menuItem',(req,res) => {
+    console.log(req.body)
+    db.collection('menu').find({menu_id:{$in:req.body.ids}}).toArray((err,result)=>{
         if(err) throw err;
         res.send(result)
     })
 })
-
 // place order 
 app.post('/placeOrder',(req,res) => {
     console.log(req.body);
